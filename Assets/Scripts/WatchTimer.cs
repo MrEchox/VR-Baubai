529a0dde2684aeb6010fadbf6bec7fc03071f676
Alanas Švažas <asalanys@gmail.com>
2024-10-28 00:32:32 +0200

    Changed doors
    Pridejau duris su valdymu kzk. Generatoriu pradejau. Pabaigiau laikroduka

--- Assets/Scripts/WatchTimer.cs
+++ Assets/Scripts/WatchTimer.cs
@@ -2,35 +2,51 @@ using UnityEngine;
 
 public class DigitalWatch : MonoBehaviour
 {
+    public int timerMinutes = 20;
+    private float minuteInterval = 60.0f; 
+
     public GameObject[] number1Bars;  // The 7 bars for the first digit
     public GameObject[] number2Bars;  // The 7 bars for the second digit
-    public Material onMaterial;       // Material to show when a bar is "on"
-    public Material offMaterial;      // Material to show when a bar is "off"
+    public Material onMaterialWatch;  
+    public Material offMaterialWatch; 
+
+    public GameObject LED;
+    public Material onMaterialLED;
+    public Material offMaterialLED;
+
+    private float flickerInterval = 0.95f;
+    private float flickerDuration = 0.05f;
+    private bool isLEDOn = true;
+
 
     // Define bar configurations for each digit (0 to 9)
     private readonly bool[][] digitPatterns = new bool[][]
     {
-        new bool[] { true, true, true, false, true, true, true },  // 0
-        new bool[] { false, false, true, false, false, true, false },  // 1
-        new bool[] { true, false, true, true, true, false, true },  // 2
-        new bool[] { true, false, true, true, false, true, true },  // 3
-        new bool[] { false, true, true, true, false, true, false },  // 4
-        new bool[] { true, true, false, true, false, true, true },  // 5
-        new bool[] { true, true, false, true, true, true, true },  // 6
-        new bool[] { true, false, true, false, false, true, false },  // 7
-        new bool[] { true, true, true, true, true, true, true },  // 8
-        new bool[] { true, true, true, true, false, true, true }   // 9
+        // Left bottom, left top, top, right top, right bottom, bottom, middle
+        new bool[] { true, true, true, true, true, true, false},            // 0
+        new bool[] { false, false, false, true, true, false, false},        // 1
+        new bool[] { true, false, true, true, false, true, true},           // 2
+        new bool[] { false, false, true, true, true, true, true},           // 3
+        new bool[] { false, true, false, true, true, false, true},          // 4
+        new bool[] { false, true, true, false, true, true, true},           // 5
+        new bool[] { true, true, true, false, true, true, true},            // 6
+        new bool[] { false, true, true, true, true, false, false},          // 7
+        new bool[] { true, true, true, true, true, true, true},             // 8
+        new bool[] { false, true, true, true, true, false, true}            // 9
+        
     };
+
+    private void Start()
+    {
+        InvokeRepeating("TurnOnLED", 0.0f, flickerInterval);
+        InvokeRepeating("DecrementMinute", 0.0f, minuteInterval);
+    }
 
     void Update()
     {
-        // Get current hour and minute
-        int hour = System.DateTime.Now.Hour;
-        int minute = System.DateTime.Now.Minute;
-
         // Get the digits
-        int firstDigit = hour / 10;
-        int secondDigit = hour % 10;
+        int firstDigit = timerMinutes / 10;
+        int secondDigit = timerMinutes % 10;
 
         // Update the bars based on the current time
         SetDigit(number1Bars, firstDigit);
@@ -45,7 +61,32 @@ public class DigitalWatch : MonoBehaviour
         for (int i = 0; i < bars.Length; i++)
         {
             Renderer barRenderer = bars[i].GetComponent<Renderer>();
-            barRenderer.material = pattern[i] ? onMaterial : offMaterial;
+            barRenderer.material = pattern[i] ? onMaterialWatch : offMaterialWatch;
         }
+    }
+
+    private void DecrementMinute()
+    {
+        timerMinutes--;
+        if ( timerMinutes < 0)
+        {
+            timerMinutes = 0;
+            CancelInvoke("DecrementMinute");
+            // Play a sound
+        }
+    }
+
+    private void TurnOnLED()
+    {
+        Renderer ledRenderer = LED.GetComponent<Renderer>();
+        ledRenderer.material = onMaterialLED;
+        Invoke("TurnOffLED", flickerDuration);
+
+    }
+    
+    private void TurnOffLED()
+    {
+        Renderer ledRenderer = LED.GetComponent<Renderer>();
+        ledRenderer.material = offMaterialLED;
     }
 }
